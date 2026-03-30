2e7a31d67ef13935aed6a0832426a3dc04839b86
stanszulc <30842830+stanszulc@users.noreply.github.com>
2026-03-30 08:55:04 +0200

    Initialize React application with ORchestratorDashboard

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ORchestratorDashboard from "./ORchestratorDashboard";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ORchestratorDashboard />
+  </React.StrictMode>
+);
